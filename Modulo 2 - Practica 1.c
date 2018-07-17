aef7138e303e494684b8a5705405de2a29c0667d
Bryan Useche <bryan.useche.10@gmail.com>
2018-07-17 09:44:15 -0400

    Update Modulo 2 - Practica 1.c
    Add the description of the problem

--- Modulo 2 - Practica 1.c
+++ Modulo 2 - Practica 1.c
@@ -1,3 +1,5 @@
+/* Realizar un programa que me permita guardar un vector de 7 posiciones, y que muestre el valor 2 y 5*/
+/* Make a program that allows me to save a vector of 7 positions, and that shows the value 2 and 5 */
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
